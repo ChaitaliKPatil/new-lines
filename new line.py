9ed502abb6e5843f47c5ddae18b0b6ebd044a1d0
ChaitaliKPatil <90540795+ChaitaliKPatil@users.noreply.github.com>
2021-09-14 21:22:17 +0530

    Create new line.py

---
+++ new line.py
@@ -0,0 +1,6 @@
+#Q2
+# attempt to display Escape Sequences
+# single, double codes, backslash, backspace, new line, Tab.
+print('Q2\nLine A \\n Line B')
+print('\\\" \n\\\'\n\n\n\n')
+# Obtaining User Input and then storing the input value to a variable using = Operator
